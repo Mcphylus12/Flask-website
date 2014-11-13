38ec28b5cb166a69484c65214d8d49abb0e992ba
Kyle Alexander <mcphylus@live.co.uk>
2014-11-13 14:44:08 +0000

    made comments server side

--- routes.py
+++ routes.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from cgi import escape #used to escape html special characters
+import time
 import csv
 
 app = Flask(__name__)
@@ -15,7 +17,24 @@ def attractions():
 
 @app.route('/comments')
 def comments():
-    return render_template('Comments.html')
+    comList = readFile("static/comments.csv")
+    return render_template('Comments.html', comList=comList, err=False)
+
+@app.route('/addComment', methods=['post'])
+def addComment():
+    uName = escape(request.form[('uName')]).replace('\n', ' ').replace('\r', ' ')
+    uComment = (request.form[('uComment')]).replace('\n', ' ').replace('\r', ' ')
+    dateStr = time.strftime("%d-%m-%y %H:%M")
+    if uName == '':
+        uName = 'Anonymous'
+    comList = readFile("static/comments.csv")
+    if uComment == '':
+        err = True
+    else:
+        err = False
+        comList.append([uName, uComment, dateStr])
+        writeFile("static/comments.csv", comList)
+    return render_template('Comments.html', comList=comList, err=err)
 
 @app.route('/contact')
 def contact():
@@ -24,6 +43,19 @@ def contact():
 @app.route('/gallery')
 def gallery():
     return render_template('Gallery.html')
+
+
+def readFile(File):
+	with open(File, 'r', newline='') as inFile:
+		reader = csv.reader(inFile)
+		comList = [row for row in reader]
+	return comList
+
+
+def writeFile(File, appendedList):
+	with open(File, 'w', newline='') as outFile:
+		writer = csv.writer(outFile)
+		writer.writerows(appendedList)
 
 
 if __name__ == '__main__':
