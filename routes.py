11d6e1ceed32f6e602ba0134075e97362b49ef39
Kyle Alexander <mcphylus@live.co.uk>
2014-11-17 17:42:37 +0000

    Got bookings working with validation

--- routes.py
+++ routes.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request
+from datetime import datetime, timedelta
 from cgi import escape #used to escape html special characters
 import csv
 import re
+import time
 
 app = Flask(__name__)
 
@@ -24,7 +26,7 @@ def comments():
 def addComment():
     uName = escape(request.form[('uName')]).replace('\n', ' ').replace('\r', ' ')
     uComment = (request.form[('uComment')]).replace('\n', ' ').replace('\r', ' ')
-    dateStr = time.strftime("%d-%m-%y")
+    dateStr = time.strftime("%d-%m-%Y")
     if uName == '':
         uName = 'Anonymous'
     comList = readFile("static/comments.csv")
@@ -46,17 +48,42 @@ def gallery():
 
 @app.route('/book')
 def book():
-    return render_template('Book.html',bookList=readFile('static/booking.csv'))
+    bookList=readFile('static/booking.csv')
+    bookList = cullPastDates(bookList)
+    return render_template('Book.html', bookList=bookList)
+
+def cullPastDates(bookList):
+    for booking in bookList:
+        bookingDate = datetime.strptime(booking[1], '%d/%m/%Y')
+        if bookingDate - datetime.now() < timedelta(0):
+            bookList.remove(booking)
+    return bookList
+
 
 @app.route('/processBooking', methods=['post'])
 def processBooking():#TODO perform regex checks on fields and set errorlist if any errors found
-    errorList = [0]
+    errorList = [0]#flag to check for see if any errors have occured
     dateRegex = re.compile('^([0-2]?[0-9]|3[0-1])/(0?[0-9]|1[0-2])/([0-9]?[0-9]?[0-9]?[0-9]?)$')
     emailRegex = re.compile('\w+@\w+\.\w+')
     startDate = '/'.join([request.form[('startDateDay')], request.form[('startDateMonth')], request.form[('startDateYear')]])
-    endDate = '/'.join([request.form[('startDateDay')], request.form[('startDateMonth')], request.form[('startDateYear')]])
+    endDate = '/'.join([request.form[('endDateDay')], request.form[('endDateMonth')], request.form[('endDateYear')]])
     email = request.form[('email')]
     name = request.form[('name')]
+    try:
+        startDateTime = datetime.strptime(startDate, '%d/%m/%Y')
+        endDateTime = datetime.strptime(endDate, '%d/%m/%Y')
+    except ValueError:
+        errorList[0] = 1
+        errorList.append('invaliddateexception')
+    else:
+        print(startDateTime, ' ', endDateTime)
+        print((startDateTime - endDateTime).days)
+        if (startDateTime - endDateTime) > timedelta(0):
+            errorList[0] = 1
+            errorList.append('leavebeforearriveexception')
+        if (startDateTime - datetime.now()) < timedelta(0) or (endDateTime - datetime.now()) < timedelta(0):
+            errorList[0] = 1
+            errorList.append('bookinpastexception')
     if not(dateRegex.match(startDate)):
         errorList[0] = 1
         errorList.append('startdateformatexception')
@@ -67,9 +94,11 @@ def processBooking():#TODO perform regex checks on fields and set errorlist if a
         errorList[0] = 1
         errorList.append('emailformatexception')
 
-    bookList = readFile('static/booking.csv')
-    bookList.append([startDate, endDate, name, email, False])
-    writeFile('static/booking.csv', bookList)
+
+    if errorList[0] == 0:
+        bookList = readFile('static/booking.csv')
+        bookList.append([startDate, endDate, name, email, False])
+        writeFile('static/booking.csv', bookList)
     return render_template('ProcessBooking.html', errorList=errorList)
 
 
